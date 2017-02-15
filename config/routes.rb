4143326ec1a4445fa43b1b1b9d8c692e65be4497
Katie Zablocki <katie.zablocki@gordon.edu>
2017-02-14 22:34:58 -0500

    password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   # main page of WriteIt
@@ -20,5 +24,6 @@ Rails.application.routes.draw do
   resources :posts
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
